9aafbcb33bd91a4d2c7a366f4a9842ab14f1be72
aldangold <meloshnikoff@gmail.com>
2021-06-30 22:38:55 +0300

    step5: added game brain-even

--- src/cli.js
+++ src/cli.js
@@ -6,6 +6,7 @@ const askUser = () => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
+  return name;
 };
 
 export default askUser;
