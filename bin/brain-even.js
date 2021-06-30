9a24f613fc2d29f8803cf5782a0cf88edff24161
aldangold <meloshnikoff@gmail.com>
2021-06-30 22:49:47 +0300

    step5: added game brain-even & fix lint

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -6,20 +6,20 @@ import askUser from '../src/cli.js';
 
 const name = askUser();
 const buildMatch = () => {
-    console.log('Answer "yes" if number even otherwise answer "no".');
-    for (let i = 0; i < 3;) {
-        const question = Math.round(Math.random() * 100);
-        console.log(`Question: ${question}`);
-        const correctAnswer = question % 2 === 0 ? 'yes' : 'no';
-        const answer = readlineSync.question('Your answer: ');
-        if (answer === correctAnswer) {
-            console.log('Correct!');
-            i += 1;
-        } else {
-            console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${name}!`);
-        }
+  console.log('Answer "yes" if number even otherwise answer "no".');
+  for (let i = 0; i < 3;) {
+    const question = Math.round(Math.random() * 100);
+    console.log(`Question: ${question}`);
+    const correctAnswer = question % 2 === 0 ? 'yes' : 'no';
+    const answer = readlineSync.question('Your answer: ');
+    if (answer === correctAnswer) {
+      console.log('Correct!');
+      i += 1;
+    } else {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${name}!`);
     }
-    console.log(`Congratulations, ${name}!`);
+  }
+  console.log(`Congratulations, ${name}!`);
 };
 
 buildMatch();
